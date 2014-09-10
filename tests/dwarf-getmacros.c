4636a124f50cb221a25513d6f9ec7ae4ebc15bf9
Petr Machata <pmachata@redhat.com>
2014-09-10 23:05:14 +0200

    Test cases for .debug_macro support
    Signed-off-by: Petr Machata <pmachata@redhat.com>

--- tests/dwarf-getmacros.c
+++ tests/dwarf-getmacros.c
@@ -1,5 +1,5 @@
-/* Test program for dwfl_module_return_value_location.
-   Copyright (C) 2009 Red Hat, Inc.
+/* Test program for dwarf_getmacros and related
+   Copyright (C) 2009, 2014 Red Hat, Inc.
    This file is part of elfutils.
 
    This file is free software; you can redistribute it and/or modify
@@ -24,6 +24,59 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <inttypes.h>
+
+static void include (Dwarf *dbg, Dwarf_Off macoff, ptrdiff_t token);
+
+static int
+mac (Dwarf_Macro *macro, void *dbg)
+{
+  static int level = 0;
+  unsigned int version = -1U;
+  dwarf_macro_version (macro, &version);
+  assert (version != -1U);
+
+  unsigned int opcode;
+  dwarf_macro_opcode (macro, &opcode);
+
+  if (version == 4 && opcode == DW_MACRO_GNU_transparent_include)
+    {
+      Dwarf_Attribute at;
+      int r = dwarf_macro_param (macro, 0, &at);
+      assert (r == 0);
+
+      Dwarf_Word w;
+      r = dwarf_formudata (&at, &w);
+      assert (r == 0);
+
+      printf ("%*sinclude %#" PRIx64 "\n", level, "", w);
+      ++level;
+      include (dbg, w, 0);
+      --level;
+      printf ("%*s/include\n", level, "");
+    }
+  else if ((version == 4 && opcode == DW_MACRO_GNU_define_indirect)
+	   || (version == 0 && opcode == DW_MACINFO_define))
+    {
+      const char *value;
+      dwarf_macro_param2 (macro, NULL, &value);
+      printf ("%*s%s\n", level, "", value);
+    }
+
+  return DWARF_CB_ABORT;
+}
+
+static void
+include (Dwarf *dbg, Dwarf_Off macoff, ptrdiff_t token)
+{
+  while ((token = dwarf_getmacros_off (dbg, macoff, mac, dbg, token)) != 0)
+    if (token == -1)
+      {
+	puts (dwarf_errmsg (dwarf_errno ()));
+	break;
+      }
+}
 
 int
 main (int argc __attribute__ ((unused)), char *argv[])
@@ -35,22 +88,29 @@ main (int argc __attribute__ ((unused)), char *argv[])
   Dwarf *dbg = dwarf_begin (fd, DWARF_C_READ);
 
   Dwarf_Die cudie_mem, *cudie = dwarf_offdie (dbg, cuoff, &cudie_mem);
-  int mac (Dwarf_Macro *macro, void *data __attribute__ ((unused)))
+
   {
-    unsigned int opcode;
-    dwarf_macro_opcode (macro, &opcode);
-    if (opcode == DW_MACINFO_define)
-      {
-	const char *value;
-	dwarf_macro_param2 (macro, NULL, &value);
-	puts (value);
-      }
-    return DWARF_CB_ABORT;
+    puts ("--dwarf_getmacros--");
+    ptrdiff_t off = 0;
+    while ((off = dwarf_getmacros (cudie, mac, dbg, off)) > 0)
+      ;
   }
 
-  ptrdiff_t off = 0;
-  while ((off = dwarf_getmacros (cudie, mac, NULL, off)) > 0)
-    ;
+  puts ("--dwarf_getmacros_die w/o cache--");
+  for (ptrdiff_t off = 0;
+       (off = dwarf_getmacros_die (cudie, NULL, mac, dbg, off)); )
+    if (off == -1)
+      {
+	puts (dwarf_errmsg (dwarf_errno ()));
+	break;
+      }
+
+  puts ("--dwarf_getmacros_die w/ cache--");
+  {
+    Dwarf_Off macoff;
+    ptrdiff_t off = dwarf_getmacros_die (cudie, &macoff, mac, dbg, 0);
+    include (dbg, macoff, off);
+  }
 
   return 0;
 }
